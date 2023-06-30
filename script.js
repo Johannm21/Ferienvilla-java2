c71f9de4c28e1dff164e0f5e515e0264c43d5ba3
Johannm21 <137768684+Johannm21@users.noreply.github.com>
2023-06-30 11:32:02 +0200

    Version 1.4

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 function handleSubmit(){
-  var Vname=document.getElementById('Vname').value;
-  var Nname=document.getElementById('Nname').value;
-  var Straße=document.getElementById('Straße').value;
-  var Hausnummer=document.getElementById('Hausnummer').value;
-  var Postleitzahl=document.getElementById('Postleitzahl').value;
-  var Ort=document.getElementById('Ort').value;
+  var vname=document.getElementById('vname').value;
+  var nname=document.getElementById('nname').value;
+  var street=document.getElementById('Straße').value;
+  var hausnummer=document.getElementById('Hausnummer').value;
+  var postleitzahl=document.getElementById('Postleitzahl').value;
+  var ort=document.getElementById('Ort').value;
   var date1=document.getElementById('date1').value;
   var date2=document.getElementById('date2').value;
-  var Mail=document.getElementById('E-Mail').value;
-  var Haustier=document.getElementById('Haustier').value;
-  var Datenschutzerklärung=document.getElementById('flexCheckIndeterminate').checked;
-}
+  var mail=document.getElementById('E-Mail').value;
+  var haustier=document.getElementById('Haustier').value;
+  var datenschutzerklaerung=document.getElementById('flexCheckIndeterminate').checked;
+
 
 var message=
   'Vorname: ' +
-  Vname +
+  vname +
   'Nname: ' +
-  Nachname +
+  nachname +
   'Straße: ' +
-  Straße +
+  street +
   'Hausnummer: ' +
-  Hausnummer +
+  hausnummer +
   'Postleitzahl: ' +
-  Postleitzahl +
+  postleitzahl +
   'Ort: ' +
-  Ort +
+  ort +
   'Anreisedatum: ' +
   date1 +
   'Abreisedatum: ' +
   date2 +
   'E-Mail: ' +
-  Mail +
+  mail +
   'Mitgebrachte Haustiere: ' +
-  Haustier +
+  haustier +
 'Datenschutzerklärung annerkannt: ' + 
-Datenschutzerklärung
+datenschutzerklaerung
 
 alert(`Folgende Angaben wurden an uns übersandt: ${message} Vielen Dank für Ihre Buchung.`);
+}
